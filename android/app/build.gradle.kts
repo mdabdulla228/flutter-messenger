7533747efe9cf7d1f8e8393d4b71c58e24eea5b7
mdabdulla228 <Rajkhanpk589@gmail.com>
2026-09-14 14:59:47 +0300

    Update compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.flutter_messenger"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.example.flutter_messenger"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
